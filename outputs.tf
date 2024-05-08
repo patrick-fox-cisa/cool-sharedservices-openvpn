fe9762e8761b233d03a0f4f9660d5b94372699bb
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-08 14:26:45 -0400

    Alphabetize

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "assessment_environment_services_access_security_group" {
-  value       = aws_security_group.assessment_environment_services_access
   description = "The security group allowing VPN users access to services running in the assessment environments."
+  value       = aws_security_group.assessment_environment_services_access
 }
 
 output "instance_id" {
-  value       = module.openvpn.id
   description = "The ID corresponding to the OpenVPN server EC2 instance."
+  value       = module.openvpn.id
 }
 
 output "security_group_id" {
-  value       = module.openvpn.security_group_id
   description = "The ID corresponding to the OpenVPN server security group."
+  value       = module.openvpn.security_group_id
 }
--- variables.tf
+++ variables.tf
@@ -4,18 +4,18 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 variable "client_dns_search_domain" {
-  type        = string
   description = "The DNS search domain to be pushed to VPN clients."
+  type        = string
 }
 
 variable "client_dns_server" {
-  type        = string
   description = "The address of the DNS server to be pushed to the VPN clients."
+  type        = string
 }
 
 variable "client_network" {
-  type        = string
   description = "A string containing the network and netmask to assign client addresses. The server will take the first address. (e.g. \"10.240.0.0 255.255.255.0\")."
+  type        = string
 }
 
 # ------------------------------------------------------------------------------
@@ -25,96 +25,96 @@ variable "client_network" {
 # ------------------------------------------------------------------------------
 
 variable "aws_region" {
-  type        = string
-  description = "The AWS region where the shared services account is to be created (e.g. \"us-east-1\")."
   default     = "us-east-1"
+  description = "The AWS region where the shared services account is to be created (e.g. \"us-east-1\")."
+  type        = string
 }
 
 variable "cert_bucket_name" {
-  type        = string
-  description = "The name of the AWS S3 bucket where certificates are stored."
   default     = "cisa-cool-certificates"
+  description = "The name of the AWS S3 bucket where certificates are stored."
+  type        = string
 }
 
 variable "client_motd_url" {
-  type        = string
-  description = "A URL to the motd page.  This will be pushed to VPN clients as an environment variable."
   default     = "https://github.com/cisagov/cool-system/blob/develop/motd.md#welcome-to-cisas-cloud-oriented-operations-lab-cool"
+  description = "A URL to the motd page.  This will be pushed to VPN clients as an environment variable."
+  type        = string
 }
 
 variable "cool_domain" {
-  type        = string
-  description = "The domain where the COOL resources reside (e.g. \"cool.cyber.dhs.gov\")."
   default     = "cool.cyber.dhs.gov"
+  description = "The domain where the COOL resources reside (e.g. \"cool.cyber.dhs.gov\")."
+  type        = string
 }
 
 variable "crowdstrike_falcon_sensor_customer_id_key" {
-  type        = string
-  description = "The SSM Parameter Store key whose corresponding value contains the customer ID for CrowdStrike Falcon (e.g. /cdm/falcon/customer_id)."
   default     = "/cdm/falcon/customer_id"
+  description = "The SSM Parameter Store key whose corresponding value contains the customer ID for CrowdStrike Falcon (e.g. /cdm/falcon/customer_id)."
+  type        = string
 }
 
 variable "crowdstrike_falcon_sensor_tags_key" {
-  type        = string
-  description = "The SSM Parameter Store key whose corresponding value contains a comma-delimited list of tags that are to be applied to CrowdStrike Falcon (e.g. /cdm/falcon/tags)."
   default     = "/cdm/falcon/tags"
+  description = "The SSM Parameter Store key whose corresponding value contains a comma-delimited list of tags that are to be applied to CrowdStrike Falcon (e.g. /cdm/falcon/tags)."
+  type        = string
 }
 
 variable "nessus_hostname_key" {
-  type        = string
-  description = "The SSM Parameter Store key whose corresponding value contains the hostname of the CDM Tenable Nessus server to which the Nessus Agent should link (e.g. /cdm/nessus/hostname)."
   default     = "/cdm/nessus_hostname"
+  description = "The SSM Parameter Store key whose corresponding value contains the hostname of the CDM Tenable Nessus server to which the Nessus Agent should link (e.g. /cdm/nessus/hostname)."
+  type        = string
 }
 
 variable "nessus_key_key" {
-  type        = string
-  description = "The SSM Parameter Store key whose corresponding value contains the secret key that the Nessus Agent should use when linking with the CDM Tenable Nessus server (e.g. /cdm/nessus/key)."
   default     = "/cdm/nessus_key"
+  description = "The SSM Parameter Store key whose corresponding value contains the secret key that the Nessus Agent should use when linking with the CDM Tenable Nessus server (e.g. /cdm/nessus/key)."
+  type        = string
 }
 
 variable "nessus_port_key" {
-  type        = string
-  description = "The SSM Parameter Store key whose corresponding value contains the port to which the Nessus Agent should connect when linking with the CDM Tenable Nessus server (e.g. /cdm/nessus/port)."
   default     = "/cdm/nessus_port"
+  description = "The SSM Parameter Store key whose corresponding value contains the port to which the Nessus Agent should connect when linking with the CDM Tenable Nessus server (e.g. /cdm/nessus/port)."
+  type        = string
 }
 
 variable "private_networks" {
-  type        = list(string)
   description = "A list of strings, each of which contains a network and netmask defining a list of subnets that exist behind the VPN server (e.g. [\"10.224.0.0 255.240.0.0\", \"192.168.100.0 255.255.255.0\"]).  This will be concatenated with the list of S3 gateway endpoint routes and the result will be pushed to the clients."
+  type        = list(string)
 }
 
 variable "provisionaccount_role_name" {
-  type        = string
-  description = "The name of the IAM role that allows sufficient permissions to provision all AWS resources in the Shared Services account."
   default     = "ProvisionAccount"
+  description = "The name of the IAM role that allows sufficient permissions to provision all AWS resources in the Shared Services account."
+  type        = string
 }
 
 variable "provisionopenvpn_policy_description" {
-  type        = string
-  description = "The description to associate with the IAM policy that allows provisioning of OpenVPN in the Shared Services account."
   default     = "Allows provisioning of OpenVPN in the Shared Services account."
+  description = "The description to associate with the IAM policy that allows provisioning of OpenVPN in the Shared Services account."
+  type        = string
 }
 
 variable "provisionopenvpn_policy_name" {
-  type        = string
-  description = "The name to assign the IAM policy that allows provisioning of OpenVPN in the Shared Services account."
   default     = "ProvisionOpenVPN"
+  description = "The name to assign the IAM policy that allows provisioning of OpenVPN in the Shared Services account."
+  type        = string
 }
 
 variable "root_disk_size" {
-  type        = number
-  description = "The size of the OpenVPN instance's root disk in GiB."
   default     = 8
+  description = "The size of the OpenVPN instance's root disk in GiB."
+  type        = number
 }
 
 variable "tags" {
-  type        = map(string)
-  description = "Tags to apply to all AWS resources created."
   default     = {}
+  description = "Tags to apply to all AWS resources created."
+  type        = map(string)
 }
 
 variable "trusted_cidr_blocks_vpn" {
-  type        = list(string)
-  description = "A list of the CIDR blocks that are allowed to access the VPN port on the VPN servers (e.g. [\"10.10.0.0/16\", \"10.11.0.0/16\"])."
   default     = []
+  description = "A list of the CIDR blocks that are allowed to access the VPN port on the VPN servers (e.g. [\"10.10.0.0/16\", \"10.11.0.0/16\"])."
+  type        = list(string)
 }
